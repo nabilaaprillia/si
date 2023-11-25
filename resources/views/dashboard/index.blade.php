fe2f9c253a4cc0989e3c12ecdd556cb1b6e2e2e0
MuhammadFarhan009 <farhan_m1@mhs.unsyiah.ac.id>
2023-11-25 19:39:35 +0700

    multi user

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -8,7 +8,7 @@
 
                 <div class="col-xxl col-xl-12">
                     <div class="card info-card customers-card">
-    
+
                     <div class="card-body">
                         <h5 class="card-title">Selamat Datang <span>| {{ auth()->user()->name }}</span></h5>
                         <div class="d-flex align-items-center">
@@ -21,10 +21,10 @@
 
                     {{-- Semua Antrian --}}
                     <div class="col-xxl col-md-3">
-                        <div class="card info-card sales-card"> 
+                        <div class="card info-card sales-card">
                             <div class="card-body">
                                 <h5 class="card-title">Semua Antrian</h5>
-            
+
                                 <div class="d-flex align-items-center">
                                 <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                                     <i class="bx bxs-group"></i>
@@ -36,9 +36,9 @@
                             </div>
                         </div>
                     </div>
-                    
+
                 </div>
-                
+
             </div>
         </div>
     </div>
